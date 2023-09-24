19a3a5bed97451f2baddb5ca7115408cc8073802
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-24 14:41:49 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,21 +1,25 @@
 """
-This test tests if the barplot generated is a PNG image
+This test tests if the barplot generated is saved in the rep
 
 """
-from PIL import Image
+import pandas as pd
+import requests
 
-def is_valid_png(file_path):
-    try:
-        img = Image.open(file_path)
-        img.verify()
-        return True
-    except (IOError, SyntaxError):
-        return False
+def test_github_file_existence():
+    name = "dani-jimlar" 
+    repo = "djl_mini_project_1"  
+    path_to_file = "source/bar_plot2.png"  
+    url = f"https://api.github.com/repos/{name}/{repo}/contents/{path_to_file}"
+    response1 = requests.get(url)
+    assert response1.status_code == 200
 
+if __name__ == "__main_
 
-file_path = "djl_project2/source/bar_plot.png"
+file_path = "djl_project2/source/bar_plot2.png"
 if is_valid_png(file_path):
     print("The image is valid PNG image.")
 else:
     print("The image is not valid PNG image.")
 
+if __name__ == "__main__":
+    test_github_file_existence()
