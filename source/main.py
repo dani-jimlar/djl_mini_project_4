aa577901e48f4edf60ba6c160b9a01eb04c3aa77
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-24 14:43:48 -0400

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,36 +1,17 @@
 """
-This fun prints descriptive analytics of a data set
+This script uses functions from lib.py to generate a brief analysis of a
+dataset and save a data viszualization of the data
 """
+import pandas as pd 
+from source import lib 
+dt= pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1")
+# Print dataset info
+lib.print_ds_info(dt)
+# view and save graph
+lib.view_data_vis(dt)
+lib.safe_data_vis2(dt)
 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
- 
-def print_ds_info(data):
-    """
-    parameter:data
-    uses data.columns and describe() to print descriptive info of dataset
-    """
-    print("The variables in this data set are the following:",list(data.columns))
-    print("Data behaves as follows:\n", data.describe())
- 
-def data_vis(data):
-    """this fun generates a simple plot of the data set"""
-    sns.barplot(data=data,  x="descrip",y="sexostt", ci=None)
-    plt.xlabel("Age group")
-    plt.ylabel("Number of inmmates")
-    plt.title("Number of inmates by age group in Mexican prisons in 2021")
-    locs, labels = plt.xticks()
-    plt.setp(labels, rotation=90)
-    plt.savefig('bar_plot.png', dpi=300, bbox_inches='tight')
+#Print descriptive statistics of just one variable.
+selected=dt.iloc[:,3]
 
-
-if __name__ == "__main__":
-    #dir=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'source'))
-    #sys.path.append(dir)
-    # Read the CSV file
-    my_data = pd.read_csv("source/imp_edos_2.csv", encoding="ISO-8859-1")
-    # Print dataset info
-    print_ds_info(my_data)
-    # generate graph
-    data_vis(my_data)
+lib.print_min(selected)
